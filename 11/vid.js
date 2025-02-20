4a6986dbb77cd719f639c56bebce7ebb1e2a43d1
Antarachat847 <chata416@newschool.edu>
2025-02-20 10:38:11 -0500

    ja bhai

--- 11/vid.js
+++ 11/vid.js
@@ -2,17 +2,20 @@ document.addEventListener("DOMContentLoaded", function() {
     const video = document.getElementById('playVideo');
     const staticImage = document.getElementById('staticImage');
     const clickArea = document.querySelector('.click-area');
-    let videoPlaying = false;
+    let videoStarted = false;
 
     clickArea.addEventListener('click', () => {
-        if (!videoPlaying) {
+        if (!videoStarted) {
             video.style.display = "block"; // Show video
             video.play(); // Start playing
             staticImage.style.display = "none"; // Hide static image
-            videoPlaying = true;
-        } else {
-            // Redirect to the next page after second click
-            window.location.href = "https://antarachat847.github.io/Water/"; 
+            videoStarted = true;
         }
     });
+
+    // Second click anywhere on the video should redirect
+    video.addEventListener('click', () => {
+        window.location.href = "https://antarachat847.github.io/Water/";
+    });
 });
+;
